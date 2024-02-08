72d1f8742d84abde451781ccc5b487d95537dd54
adarshgnath <64641017+adarshgnath@users.noreply.github.com>
2024-02-08 10:55:12 +0530

    20 restructure root modules (#22)
    * Update week1.md
    
    * #20-restructure root modules, made terraform cloud-->local

--- main.tf
+++ main.tf
@@ -1,45 +1,19 @@
-terraform {
-
-  cloud {
-    organization = "KUMOFY"
-
-    workspaces {
-      name = "terrahouse-1"
-    }
-  }
-  
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.6.0"
-    }
-
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.33.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
-  length           = 32
-  special          = false
+  length   = 32
+  special  = false
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 
+  tags = {
+    UserUuid  = var.user_uuid 
   }
-
-output "Random_bucket_name" {
-  value = random_string.bucket_name.result
 }
 
