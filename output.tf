79e73e3793af2ef75edc64ee3e17a1381b6f7391
adarshgnath <adarshgnathadoor@gmail.com>
2024-02-18 18:15:09 +0000

    33 cloudfront distribution cache purge when changinging content_version

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "website_endpoint" {
   description = "endpint for S3 static website hosting"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {  
+  description = "Cloudfront URL"
+  value = module.terrahouse_aws.cloudfront_url
+}
