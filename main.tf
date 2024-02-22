547afe1ce2185282d6d13ac4cfb81df03cd40fa7
adarshgnath <adarshgnathadoor@gmail.com>
2024-02-22 10:08:07 +0000

    #39 added local terraform provider to main.tf, ran tf apply__>all good

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 
 
 terraform {
+
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+}
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "ExamPro"
@@ -15,14 +23,20 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+# }
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   path_index_html = var.path_index_html
+#   path_error_html = var.path_index_html
+#   content_variable = var.content_variable
+#   assets_path = var.assets_path
 
-}
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  path_index_html = var.path_index_html
-  path_error_html = var.path_index_html
-  content_variable = var.content_variable
-  assets_path = var.assets_path
-}
+
+# }
--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "website_endpoint" {  
-  description = "endpint for S3 static website hosting"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "website_endpoint" {  
+#   description = "endpint for S3 static website hosting"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {  
-  description = "Cloudfront URL"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {  
+#   description = "Cloudfront URL"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
